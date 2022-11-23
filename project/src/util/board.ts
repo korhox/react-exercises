9db374462cd6ca5fdf82886316142ab4eeea586c
korhox <juuso@korho.fi>
2022-11-23 22:22:06 +0200

    chore: :label: Update types for TS

--- project/src/util/board.ts
+++ project/src/util/board.ts
@@ -1,12 +1,33 @@
 import { Issue } from "./issue";
+import { v4 as uuidv4 } from "uuid";
 
 export class Board {
-    public readonly id: BigInteger;
-    public readonly name: string;
-    public readonly issues: Issue[] = [];
+    private readonly _id: string;
+    private _name: string;
+    private _issues: Issue[] = [];
 
-    constructor(id: BigInteger, name: string) {
-        this.id = id;
-        this.name = name;
+    constructor(name: string) {
+        this._id = uuidv4();
+        this._name = name;
+    }
+
+    public get id(): string {
+        return this._id;
+    }
+
+    public get issues(): Issue[] {
+        return this._issues;
+    }
+
+    public set issues(value: Issue[]) {
+        this._issues = value;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public set name(value: string) {
+        this._name = value;
     }
 }
--- project/src/util/issue.ts
+++ project/src/util/issue.ts
@@ -1,13 +1,29 @@
 import { Board } from "./board";
+import { v4 as uuidv4 } from "uuid";
 
 export class Issue {
-    public readonly id: BigInteger;
-    public readonly content: string;
-    public readonly board: Board;
+    private readonly _id: string;
+    private _content: string;
+    private _board: Board;
 
-    constructor(id: BigInteger, content: string, board: Board) {
-        this.id = id;
-        this.content = content;
-        this.board = board;
+    constructor(content: string, board: Board) {
+        this._id = uuidv4();
+        this._content = content;
+        this._board = board;
+    }
+    public get id(): string {
+        return this._id;
+    }
+    public get content(): string {
+        return this._content;
+    }
+    public set content(value: string) {
+        this._content = value;
+    }
+    public get board(): Board {
+        return this._board;
+    }
+    public set board(value: Board) {
+        this._board = value;
     }
 }
