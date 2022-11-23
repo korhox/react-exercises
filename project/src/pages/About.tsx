001f49df423c44ca78bf771149f269b051a188ad
korhox <juuso@korho.fi>
2022-11-23 18:28:33 +0200

    fix: :pencil2: Use correct about page info

--- project/src/pages/About.tsx
+++ project/src/pages/About.tsx
@@ -4,9 +4,9 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 const About = () => (
     <>
         <div className="container mx-auto flex justify-between p-2 mt-8 mb-4">
-            <h2 className="text-2xl">About</h2>
+            <h2 className="text-2xl">Home</h2>
             <button className="flex items-center p-2 px-4 bg-viol hover:contrast-150 text-white rounded-lg shadow">
-                <FontAwesomeIcon icon={faGithub} color="#fff" className="pr-2" />
+                <FontAwesomeIcon icon={faGithub} color="#fff" className="pr-3" />
                 <span>Open GitHub</span>
             </button>
         </div>
